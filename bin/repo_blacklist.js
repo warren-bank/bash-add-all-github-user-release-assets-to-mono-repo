a7862fea3ec505999e8e0637ba74521bfee6a2b2
Warren R. Bank <warren.r.bank@gmail.com>
2025-02-12 12:16:36 -0800

    [demo] update repo blacklist

--- bin/repo_blacklist.js
+++ bin/repo_blacklist.js
@@ -1,3 +1,8 @@
 const blacklist = []
 
+blacklist.push('mirror-Android-MultiApp')
+blacklist.push('mirror-Android-twoyi')
+blacklist.push('mirror-Android-winlator')
+blacklist.push('mirror-electron-permaweb-desktop')
+
 module.exports = blacklist
